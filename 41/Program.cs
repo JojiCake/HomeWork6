23e690b4c6ba053bce182c54f259f69256e39107
egor-turaev <egor-turaev@mail.ru>
2022-08-29 14:54:39 +0300

    Save6

---
+++ 41/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите числа через пробел: ");
+int[] array =  Console.ReadLine().Split().Select(int.Parse).ToArray();
+int result = 0;
+ 
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] > 0)
+    {
+        result++;
+    }
+}
+ 
+Console.WriteLine(result);
---
+++ 43/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("Введите значение k1: ");
+string? numberString1 = Console.ReadLine();
+int k1 = int.Parse(numberString1!);
+
+Console.WriteLine("Введите значение b1: ");
+string? numberString2 = Console.ReadLine();
+int b1 = int.Parse(numberString2!);
+
+Console.WriteLine("Введите значение k2: ");
+string? numberString3 = Console.ReadLine();
+int k2 = int.Parse(numberString3!);
+
+Console.WriteLine("Введите значение b2: ");
+string? numberString4 = Console.ReadLine();
+int b2 = int.Parse(numberString4!);
+
+if(k1 == k2)
+    {
+        Console.WriteLine("Точек пересечения не существует");
+    }
+else
+    {
+    double x = (b2 - b1) / (k1 - k2); double fX = k1 * x + b1;
+    Console.WriteLine(x);
+    }
+
